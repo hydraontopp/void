306ae38454043ee894f81d4087193565fd3ce595
voidedman <92179972+voidedman@users.noreply.github.com>
2021-10-08 14:59:02 -0400

    hi

--- Void-Public/Void/ClientBase/UIBase/Overlay.cs
+++ Void-Public/Void/ClientBase/UIBase/Overlay.cs
@@ -276,7 +276,7 @@ namespace Void.ClientBase.UIBase
 
                 switch (mod.category)
                 {
-                    case "Flies":
+                    case "Flys":
                         panel7.Controls.Add(moduleButton);
                         break;
                     case "Visual":
