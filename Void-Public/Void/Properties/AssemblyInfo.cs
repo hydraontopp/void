6b4baa3053495eed1a861a1db94edc311dee69fa
hydraontopp#0009 <90989258+hydra0nt0pp@users.noreply.github.com>
2021-10-09 15:25:59 -0400

    fixed

--- Void-Public/Void/Properties/AssemblyInfo.cs
+++ Void-Public/Void/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Void")]
+[assembly: AssemblyTitle("Voi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
